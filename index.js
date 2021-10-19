841964bbc956c7950045c6e83e6d50fd74ddf19c
priscilla <pdjarbeng@outlook.com>
2021-10-18 21:45:34 -0400

    commit tree ab7ff3fff0d46b25bce6e3153198a59414de3bfa

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// const input = document.querySelector('input');
+const input = document.querySelector('input');
 
-// function addingEventListener() {
-//   input.addEventListener('click', function(event) {
+function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!')
+  });
+}
+
+
+// //mature functions
+
+// const input = document.querySelector('input')
+// input.addEventListener('click', function(event) {
 //     return "clicked"
 //   });
-// }
-
-
-//mature functions
-
-const input = document.querySelector('input')
-input.addEventListener('click', function(event) {
-    return "clicked"
-  });
 
